0d1060a5fa2c28ff82bdbd382a58c6e60178e604
marc-staiger <101580905+marc-staiger@users.noreply.github.com>
2022-04-07 13:44:24 +0200

    Highscore bearbeitet
    Ppunktzahl Klasse erstellt
    Klasse Highscore erstellt
    Array von Punktzahl erstellt

---
+++ Highscore/src/Highscore.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Highscore {
+private ArrayList<Punktzahl> Score ;
+
+public  Highscore()
+{
+	Score = new ArrayList<Punktzahl>();
+	
+}
+
+
+public void setNewHighScore(Punktzahl neuePKT)
+
+{
+  Score.add(neuePKT);
+}
+
+public ArrayList<Punktzahl> SortiereHighscore(ArrayList<Punktzahl> zuSortierendeListe)
+{
+
+	for (Punktzahl Score)
+	pkt.get(i);
+	 
+	return Score;
+}
+}
+
---
+++ Highscore/src/Punktzahl.java
@@ -0,0 +1,23 @@
+
+public class Punktzahl {
+	private double Punktzahl;
+	private String Name;
+	
+	public Punktzahl (double Punktzahl, String Name)
+	
+	{
+	this.Punktzahl=Punktzahl;
+	this.Name=Name;
+	}
+	
+	public double getPunktzahl()
+	{
+	return Punktzahl;	
+	}
+	public String  getName()
+	{
+		return Name;
+
+	}
+	
+}
