84f3cd987cde422614443cbe5adfe639197bacfb
marc-staiger <101580905+marc-staiger@users.noreply.github.com>
2022-04-08 09:38:12 +0200

    Implementierung der klasse Highscore
    -Logik zur sortiereung begonnen
    -ArrayList eingefügt

---
+++ Mastermind Spiel/Gruppe Highscore (Marc, Timo)/Highscore/src/Highscore.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.text.*;
+
+public class Highscore {
+private ArrayList<Punktzahl> Scores ;
+ Punktzahl sample;
+
+public  Highscore()
+{
+	Scores = new ArrayList<Punktzahl>();
+	
+}
+
+
+public void setNewHighScore(Punktzahl Score)
+
+{
+  Scores.add(Score);
+}
+
+public ArrayList<Punktzahl> SortiereHighscore(ArrayList<Punktzahl> zuSortierendeListe)
+{
+	ArrayList<Punktzahl> Sortierteliste ;
+	Sortierteliste = new ArrayList<Punktzahl>();
+	
+	
+	
+	return Sortierteliste;
+}
+}
+
